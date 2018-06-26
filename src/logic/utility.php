f9ff346cfc974dacc4b53a1cf427c2073e6e3d6a
SK <SK@192.168.2.112>
2018-06-26 16:19:05 +0900

    入室管理機能のためにメソッド追加

--- src/logic/utility.php
+++ src/logic/utility.php
@@ -41,4 +41,91 @@ function checkLoginNG(){
     }
 }
 
+
+function getPDO(){
+    require_once '../hidden/DBaccess.php';
+    $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
+    $pdo = new PDO($dsn,$user,$password);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $pdo;
+}
+
+function deleteLogoutUser(){
+    /*
+     * 1.loginManageテーブルの全レコードを取得する？
+     *   それとも、SQL分で30分経過しているレコードを取得する？
+     *   時間の計算ってできるのかな？
+     *   できるけど、SQLで実行するのはちょっと違うかな。
+     *   そのまま取得する。
+     *
+     * 2.全レコードをDateTime::diffで比較して３０分経過しているレコードの
+     *   primarykeyを配列に格納する。
+     *
+     * 3.格納している配列をすべて削除する。
+     *
+     *
+     *
+     *
+     */
+
+    $pdo = getPDO();
+    $sql = 'select * from LoginManagement';
+    $stm = $pdo->prepare($sql);
+    $stm->execute();
+
+    $selectResult = $stm->fetchAll(PDO::FETCH_ASSOC);
+    $tableTimestamp = null;
+    $now = new DateTime();
+    $deleteTargetRecords =[];
+
+    foreach($result as $row){
+        $diff = $now->diff(new DateTime($row['LoginTime']))->format('%i');
+        if($diff > 30){
+            $deleteTargetRecords[] = $row['UserName'];
+        }
+    }
+
+    //削除用のロジックをつくるところから始める
+
+    $pdo = null;
+
+
+
+}
+function insertInLoginMngTable($userName){
+
+    $pdo = getPDO();
+
+    $date = new DateTime();
+
+    $sql = "insert LoginManagement(UserName, LoginTime) values ('".$userName."', '".date_format($date, 'Y-m-d H:i:s.u')."')";
+
+    $stm = $pdo->prepare($sql);
+    $stm->execute();
+
+    $pdo = null;
+
+}
+function existUserInLoginMngTable($userName){
+    $resut = false;
+
+    $pdo = getPDO();
+
+    $sql = "select * from LoginManagement where UserName = '".$userName."'";
+
+    $stm = $pdo->prepare($sql);
+    $stm->execute();
+
+    $pdo = null;
+
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+    if(count($result)==1){
+        $result = true;
+    }
+
+    return $result;
+}
+
 ?>
